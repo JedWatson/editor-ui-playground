c2397009e5fb02cea3f4cbaa014572fbb44ec897
Jed Watson <872310+JedWatson@users.noreply.github.com>
2023-08-13 23:48:05 +1000

    Make useStickyState safe for server-side

--- src/lib/useStickyState.ts
+++ src/lib/useStickyState.ts
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 
 export function useStickyState(defaultValue: any, key: string) {
   const [value, setValue] = useState(() => {
+    if (typeof localStorage === 'undefined') return defaultValue;
     const stickyValue = localStorage.getItem(key);
     return stickyValue !== null &&
       typeof stickyValue === 'string' &&
